8d5f7683aefbb14b2ecacaa716b8069f86a0741a
sheldon6660 <107275893+sheldon6660@users.noreply.github.com>
2022-07-17 10:33:00 -0600

    created pokemonList array
    created an array for the variable 'pokemonList', listing 4 different pokemon names, their heights, and their types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let faveFood = 'Ramen';
-document.write(faveFood);
+let pokemonList = [
+	{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+	{name: 'Lucario', height: 12, types: ['steel', 'fighting']},
+	{name: 'Charizard', height: 17, types: ['fire', 'flying']},
+	{name: 'Dragonite', height: 22, types: ['dragon', 'flying']},
+];
