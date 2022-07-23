f69c0a0f585ac8667117c890c67ca6d81c9843b0
sheldon6660 <107275893+sheldon6660@users.noreply.github.com>
2022-07-23 10:41:16 -0600

    added if else statements to scripts.js
    added if else for different pokemon heights. also reorganized pokemonList for better readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,33 @@
 let pokemonList = [
-	{name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-	{name: 'Lucario', height: 12, types: ['steel', 'fighting']},
-	{name: 'Charizard', height: 17, types: ['fire', 'flying']},
-	{name: 'Dragonite', height: 22, types: ['dragon', 'flying']},
+	{
+		name: 'Bulbasaur',
+		height: 7,
+		types: ['grass', 'poison']
+	},
+	{
+		name: 'Lucario',
+		height: 12,
+		types: ['steel', 'fighting']
+	},
+	{
+		name: 'Charizard',
+		height: 17,
+		types: ['fire', 'flying']
+	},
+	{
+		name: 'Dragonite',
+		height: 22,
+		types: ['dragon', 'flying']
+	},
 ];
 
+// loop that iterates over pokemonList and writes names and heights
 for (let i = 0; i < pokemonList.length; i++) {
-	document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") ")
+	if (pokemonList[i].height > 15) {
+		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - Wow, that\'s a big Pokemon!')
+	} else if (pokemonList[i].height <= 15 && pokemonList[i].height > 10) {
+		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - That\'s a normal size.')
+	} else	if (pokemonList[i].height < 10) {
+		document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ') - That\'s a tiny Pokemon!')
+	}
 }
