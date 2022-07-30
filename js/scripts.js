0bf42d9da234567ddd3bb65a0d67d3b735519104
sheldon6660 <107275893+sheldon6660@users.noreply.github.com>
2022-07-30 13:08:23 -0600

    convert pokemonList to IIFE
    converted pokemonList to IIFE named pokemonRepository, updated forEach loop to call on pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,45 @@
-let pokemonList = [
-	{
-		name: 'Bulbasaur',
-		height: 7,
-		types: ['grass', 'poison']
-	},
-	{
-		name: 'Lucario',
-		height: 12,
-		types: ['steel', 'fighting']
-	},
-	{
-		name: 'Charizard',
-		height: 17,
-		types: ['fire', 'flying']
-	},
-	{
-		name: 'Dragonite',
-		height: 22,
-		types: ['dragon', 'flying']
-	},
-];
+let pokemonRepository = (function () {
+	let pokemonList = [
+		{
+			name: 'Bulbasaur',
+			height: 7,
+			types: ['grass', 'poison']
+		},
+		{
+			name: 'Lucario',
+			height: 12,
+			types: ['steel', 'fighting']
+		},
+		{
+			name: 'Charizard',
+			height: 17,
+			types: ['fire', 'flying']
+		},
+		{
+			name: 'Dragonite',
+			height: 22,
+			types: ['dragon', 'flying']
+		},
+	]
+
+	function add(pokemon) {
+		if (typeof pokemon === 'object') {
+			if (Object.keys(pokemonList[0]).every((key) => key in item));
+			pokemonList.push(pokemon);
+		} else {
+			alert('Pokemon is invalid!')
+		}
+	}
+
+	function getAll() {
+		return pokemonList;
+	}
+
+	return {
+		add: add,
+		getAll: getAll,
+	};
+})();
 
 // for loop that iterates over pokemonList and writes names and heights
 /*for (let i = 0; i < pokemonList.length; i++) {
@@ -33,7 +53,7 @@ let pokemonList = [
 }*/
 
 // forEach loop that iterates over pokemonList and writes names and heights
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
 	document.write('<p>' + pokemon.name + ' is ' + pokemon.height + ' units tall, and is of the ' + pokemon.types[0] + ' type. ')
 	document.write(pokemon.name + ' is also of the ' + pokemon.types[1] + ' type. ' + '</p>')
 })
