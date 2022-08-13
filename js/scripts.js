3dc872cb99eb30b2c26abe2172e611145e411181
sheldon6660 <107275893+sheldon6660@users.noreply.github.com>
2022-08-13 09:56:35 -0600

    cleaned up code
    cleaned up code in scripts.js, moving element creations into IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -23,34 +23,42 @@ let pokemonRepository = (function () {
 	];
 
 	function add(pokemon) {
-		if (typeof pokemon === 'object') {
-			if (Object.keys(pokemonList[0]).every((key) => key in pokemon)); {
-				pokemonList.push(pokemon);
-			} else {
-				alert('Pokemon is invalid!');
-			}
-		}
+	    if (
+	    	typeof pokemon === "object" &&
+	    	"name" in pokemon &&
+	    	"height" in pokemon &&
+	    	"types" in pokemon
+	    ) {
+	    	repository.push(pokemon);
+	    } else {
+	    	console.log("pokemon is not correct");
+	    }
 	}
 
 	function getAll() {
 		return pokemonList;
 	}
 
+	function addListItem(pokemon) {
+		let list = document.querySelector('.list');
+		let listItem = document.createElement('li');
+		let button = document.createElement('button');
+		button.innerText = pokemon.name;
+		button.classList.add('button');
+		listItem.appendChild(button);
+		list.appendChild(listItem);
+	}
+
 	return {
 		add: add,
 		getAll: getAll,
+		addListItem: addListItem
 	};
-
 })();
 
 
 // forEach loop that iterates over pokemonList and writes names and heights
 pokemonRepository.getAll().forEach(function(pokemon) {
-	let list = document.querySelector('ul')
-	let listItem = document.createElement('li');
-	let button = document.createElement('button');
-	button.innerText = pokemon.name
-	button.classList.add('button')
-	listItem.appendChild(button)
-	list.appendChild(listItem)
-})();
+	pokemonRepository.addListItem(pokemon);
+});
+
